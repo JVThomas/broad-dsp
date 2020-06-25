7d0c972f80961acd8d83bb42fe424e8de0f28899
Justin Thomas <jthomas0209@gmail.com>
2020-06-25 07:21:40 -0400

    server configurations to point to react build, updated readme

--- server/routes.js
+++ server/routes.js
@@ -7,7 +7,7 @@ router.get('/api/subway', processSubwayData);
 
 //make sure index.html is send if root route is accessed
 router.all('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+    res.sendFile(path.join(__dirname, '../build', 'index.html'));
 });
 
 module.exports = router;
--- server/start.js
+++ server/start.js
@@ -2,12 +2,14 @@ const _ = require('lodash');
 const express = require('express');
 const routes = require('./routes');
 const morgan = require('morgan');
+const path = require('path');
 
 let app = express();
 
 if(process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
 }
+app.use(express.static(path.join(__dirname, '..', 'build')))
 app.use(morgan('dev'));
 app.use(routes);
 
